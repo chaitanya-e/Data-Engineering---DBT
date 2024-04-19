bdaf070bd4c92fdff4f98d6cb8297735fbb3f38b
saichaitanya.e <saichaitanya.e@pditechnologies.com>
2024-04-19 18:43:10 +0530

    entrypoint update

---
+++ custom_postgres/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Exit script on any error
+set -e
+
+# Execute dbt run
+echo    "Executing dbt run"
+dbt run --profiles-dir /root --project-dir /dbt
+
+# Execute dbt test
+echo    "Executing dbt test"
+dbt test --profiles-dir /root --project-dir /dbt
+
+# Generate dbt docs
+echo "Generating dbt docs..."
+dbt docs generate --profiles-dir /root --project-dir /dbt
+
+# Serve dbt docs
+# This will start a server, it should be the last command unless you run it in the background
+echo "Serving dbt docs on port 8080..."
+dbt docs serve --profiles-dir /root --project-dir /dbt --port 8080
+
+# Execute any additional commands passed to the container
+exec "$@"
